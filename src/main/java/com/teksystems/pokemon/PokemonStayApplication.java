253369795b0e19c6922e3ee207c8c7e3c5468f93
ilikebuttons <alex.daniel.ertl@gmail.com>
2022-04-10 16:54:49 -0500

    Initial Commit

---
+++ src/main/java/com/teksystems/pokemon/PokemonStayApplication.java
@@ -0,0 +1,13 @@
+package com.teksystems.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonStayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teksystems/pokemon/PokemonStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksystems.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
